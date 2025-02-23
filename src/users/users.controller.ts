2027a1102bfa135a4f823ebee4310b776770750f
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-02-22 21:57:33 -0300

    fix: multiplas imagens

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UseInterceptors, UploadedFile, UploadedFiles } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'node:path'
 import * as fs from 'node:fs/promises'
 import { randomUUID } from 'node:crypto';
@@ -16,12 +16,27 @@ export class UsersController {
     const mimeType = file.mimetype;
     const fileExtension = path.extname(file.originalname).toLowerCase().substring(1)
     const fileName = `${randomUUID()}.${fileExtension}`
-    //const fileName = fileExtension
-    console.log(fileName)
 
     const fileLocale = path.resolve(process.cwd(), 'files', fileName)
 
     await fs.writeFile(fileLocale, file.buffer)
+
+    return true
+  }
+
+  // Upload Multiplos
+  @UseInterceptors(FilesInterceptor('file'))
+  @Post('uploads')
+  async uploadsUser(
+    @UploadedFiles() files: Array<Express.Multer.File>
+  ){
+    files.forEach(async file => {
+      const fileExtension = path.extname(file.originalname).toLowerCase().substring(1)
+      const fileName = `${randomUUID()}.${fileExtension}`
+      const fileLocale = path.resolve(process.cwd(), 'files', fileName)
+      
+      await fs.writeFile(fileLocale, file.buffer)
+    })
 
     return true
   }
