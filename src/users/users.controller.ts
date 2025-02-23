24e70538b1db66301bef694c8ff4107b9832504c
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-02-22 23:53:09 -0300

    fix: atualizado link retorno da api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,11 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'node:path'
 import * as fs from 'node:fs/promises'
 import { randomUUID } from 'node:crypto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService){}
 
   // Upload 1 :: Rota sem validação com name image
   @UseInterceptors(FileInterceptor('file'))
@@ -14,16 +16,7 @@ export class UsersController {
   async uploadUser(
     @UploadedFile() file: Express.Multer.File
   ){
-    const mimeType = file.mimetype;
-    const fileExtension = path
-          .extname(file.originalname)
-          .toLowerCase().substring(1)
-    const fileName = `image.${fileExtension}`
-
-    const fileLocale = path.resolve(process.cwd(), 'files', fileName)
-
-    await fs.writeFile(fileLocale, file.buffer)
-    return true
+    return this.userService.upload1(file)
   }
 
   // Upload 2 :: Rota com validação name roudend
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as path from 'node:path'
+import * as fs from 'node:fs/promises'
+import { randomUUID } from 'node:crypto';
 
 @Injectable()
 export class UsersService {
@@ -22,5 +25,19 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async upload1(file: Express.Multer.File){
+    const imageNumber = Date.now();
+    const mimeType = file.mimetype;
+    const fileExtension = path
+          .extname(file.originalname)
+          .toLowerCase().substring(1)
+    const fileName = `image.${imageNumber}.${fileExtension}`
+    const fileLocale = path.resolve(process.cwd(), 'files', fileName)
+
+    await fs.writeFile(fileLocale, file.buffer)
+
+    return { "url" : "http://localhost:8080/files/"+fileName }
   }
 }
